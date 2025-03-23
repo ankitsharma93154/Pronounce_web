b25703113baf32b52c4952d1813e417c9ee1f23a
ankitsharma93154 <ankitsharma93154@gmail.com>
2025-03-23 12:34:03 +0530

    seo

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,7 +2,7 @@ import React, { memo } from "react";
 const Hero = memo(() => (
   <div className="hero">
     <h1 className="hero-title">
-      Master Pronunciation with Clear American & British Accents
+      Master Pronunciation with American & British Accents
     </h1>
     <p className="hero-subtitle">
       Get accurate, high-quality word pronunciations in American and British
